27774359625b3a57f189fbe5771bc7ab5e825400
Sati Line <alina.alinka.goryunova@mail.ru>
2026-03-15 14:27:30 +0700

    feat: добавлен эндпоинт /items/{item_id} с query параметром

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
