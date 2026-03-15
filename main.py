18b8992546bef58fe774b40e6854817251c8121a
Sati Line <alina.alinka.goryunova@mail.ru>
2026-03-15 14:41:25 +0700

    feat: добавлена модель Item и POST /items/

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items/", response_model=Item)
+def create_item(item: Item):
+    return item
+
